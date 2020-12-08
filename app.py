be6442b151966b0e0bef07ec1195146a3acc2a7b
Gorny <agorny@uiowa.edu>
2020-12-08 17:05:39 -0600

    Navigation
    Navigation Established

--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/availability')
+def availability():
+    return render_template('availability.html')
+    
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
