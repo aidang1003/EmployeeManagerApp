3e0fe48f256b11d0e8ecf869c04329d54c88b22d
Gorny <agorny@uiowa.edu>
2020-12-08 18:13:09 -0600

    html
    entered in form templates for html pages

--- app.py
+++ app.py
@@ -1,7 +1,84 @@
-from flask import Flask
-from flask import render_template
+import os
+from dotenv import load_dotenv
+import pymongo
+import datetime
+from bson.objectid import ObjectId
+from flask import Flask, request, render_template, redirect, url_for, session, flash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+import bcrypt
+from functools import wraps
 
 app = Flask(__name__)
+'''
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+## necessary for python-dotenv ##
+APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+dotenv_path = os.path.join(APP_ROOT, '.env')
+load_dotenv(dotenv_path)
+
+mongo = os.getenv('MONGO')
+
+client = pymongo.MongoClient(mongo)
+
+db = client['EmployeeSchedulingManager']
+
+users = db['users']
+roles = db['roles']
+recipes = db['employee_availability']
+categories = db['schedule']
+
+    
+# Python wrappers that determine role access 
+def roles_required(*role_names):
+    def decorator(original_route):
+        @wraps(original_route)
+        def decorated_route(*args, **kwargs):
+            if not current_user.is_authenticated:
+                print('The user is not authenticated.')
+                return redirect(url_for('login'))
+            
+            print(current_user.role)
+            print(role_names)
+            if not current_user.role in role_names:
+                print('The user does not have this role.')
+                return redirect(url_for('login'))
+            else:
+                print('The user is in this role.')
+                return original_route(*args, **kwargs)
+        return decorated_route
+    return decorator
+
+
+@login.user_loader
+def load_user(username):
+    u = users.find_one({"email": username})
+    if not u:
+        return None
+    return User(username=u['email'], role=u['role'], id=u['_id'])
+
+class User:
+    def __init__(self, id, username, role):
+        self._id = id
+        self.username = username
+        self.role = role
+
+    @staticmethod
+    def is_authenticated():
+        return True
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_anonymous():
+        return False
+
+    def get_id(self):
+        return self.username
+'''
+
 
 @app.route('/')
 def index():
